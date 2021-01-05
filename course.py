a1e33a69fa845c7b381288376f6838d829487c89
mikezzb <mikezhou96@gmail.com>
2021-01-05 19:28:11 +0800

    Parse all terms and course outcome

--- course.py
+++ course.py
@@ -39,18 +39,24 @@ class Course:
                 im.show()
                 captcha = str(input('Input the captcha here: '))
             else:
-                captcha = pytesseract.image_to_string(im)
+                captcha = pytesseract.image_to_string(im) # not working lol, need to do CNN later
                 print(f'Recognized captcha: {captcha}')
             return captcha
 
-    def update_form(self, soup: BeautifulSoup) -> dict:
+    def update_form(self, soup: BeautifulSoup, update=True, additional_keys=None) -> dict:
         form_body = {'__VIEWSTATEFIELDCOUNT': soup.select_one('#__VIEWSTATEFIELDCOUNT')['value']}
         form_keys = ['__EVENTVALIDATION', '__VIEWSTATEGENERATOR', '__VIEWSTATE'] + [f'__VIEWSTATE{str(i)}' for i in range(1, int(form_body['__VIEWSTATEFIELDCOUNT']))]
+        if additional_keys:
+            form_keys.extend(additional_keys)
         for k in form_keys:
-            form_body[k] = soup.select_one(f'#{k}')['value']
-        self.form_body.update(form_body)
+            id = k.replace('$', '_')
+            form_body[k] = soup.select_one(f'#{id}')['value']
+        if update:
+            self.form_body.update(form_body)
+        else:
+            return form_body
 
-    def search_subject(self, subject, save=False, manual=True):
+    def search_subject(self, subject, save=False, manual=True): # get all course under a subject code
         with closing(self.sess.get(self.course_url, headers=self.headers)) as res:
             soup = BeautifulSoup(res.text, 'html.parser')
             self.update_form(soup)
@@ -72,7 +78,7 @@ class Course:
             with closing(self.sess.post(self.course_url, headers=self.form_headers, data=form_body)) as res:
                 self.parse_subject_courses(subject, res.text, save)
     
-    def parse_subject_courses(self, subject, html, save):
+    def parse_subject_courses(self, subject, html, save): # parse the courses under a subject and get details of each course
         course_list = []
         soup = BeautifulSoup(html, 'html.parser')
         self.update_form(soup)
@@ -95,13 +101,13 @@ class Course:
                 course.update(course_detail)
             course_list.append(course)
         course_list = sorted(course_list, key=lambda x:x['code'])
-
         if save:
             with open(f'{subject}.json', 'w') as f:
                 json.dump(course_list, f)
         self.courses[subject] = course_list
 
     def parse_course_detail(self, html) -> dict:
+        # Get general information about the course
         soup = BeautifulSoup(html, 'html.parser')
         course_detail = {
             'career': soup.select_one('#uc_course_lbl_acad_career').text,
@@ -116,41 +122,87 @@ class Course:
         }
         if course_detail['requirements']:
             course_detail['requirements'] = soup.select_one('#uc_course_tc_enrl_requirement').get_text(';')
-
+        
+        # Get sections of the course
         try: 
-            course_sections = {}
-            course_sections_table = soup.select_one('#uc_course_gv_sched').findChildren('tr', recursive=False)
-            course_sections_table.pop(0) # remove the header node
-            for schedule in course_sections_table:
-                # schedule is a <tr> tag with 3 children, first is the section code, second is the reg status, last is course detail table
-                children = schedule.findChildren('td', recursive=False)
-                section = children[0].text.strip('\n')
-                start_times, end_times, days, locations, instructors, meeting_dates = [], [], [], [], [], ''
-                timeslots = children[2].findChildren('tr') # each tr is a teaching timeslot, e.g. Wed and Thu Lectures are two teaching timelot
-                for node in timeslots:
-                    details = list(filter(lambda x: x!='\n', node.get_text(';').split(';'))) # 0: days & time 1: Room 2: Instructor 3: part of teaching date
-                    days_and_times = self.parse_days_and_times(details[0])
-                    if days_and_times[0] in days and days_and_times[1] in start_times: # i.e. duplicated
-                        continue
-                    days.append(days_and_times[0])
-                    start_times.append(days_and_times[1])
-                    end_times.append(days_and_times[2])
-                    locations.append(details[1])
-                    instructors.append(details[2])
-                    meeting_dates += f', {details[3]}'
-                course_sections[section] = {
-                    'startTimes': start_times,
-                    'endTimes': end_times,
-                    'days': days,
-                    'locations': locations,
-                    'instructors': instructors
-                }
-            course_detail['sections'] = course_sections
-            print(course_sections)
+            term_selection_options = soup.select_one('#uc_course_ddl_class_term').findChildren('option', recursive=False)
+            for term_node in term_selection_options:
+                if term_node.has_attr('selected'):
+                    print(f"For term {term_node.text}")
+                    course_sections = self.parse_sections(soup)
+                else:
+                    # post form request to get schedule for non-default term
+                    print(f"For non-selected term {term_node.text}")
+                    form = {
+                        'uc_course$btn_class_section': 'Show sections',
+                        'uc_course$ddl_class_term': term_node['value'],
+                    }
+                    form.update(self.update_form(soup, update=False))
+                    with closing(self.sess.post(self.course_url, headers=self.headers, data=form)) as res:
+                        soup_alt = BeautifulSoup(res.text, 'html.parser')
+                        course_sections = self.parse_sections(soup_alt)
+                    pass
+                course_detail[term_node.text] = course_sections
+                print(course_sections)
+            
+            # Get course outcome for the course
+            form = {
+            'btn_course_outcome': 'Course Outcome',
+            'uc_course$ddl_class_term': term_selection_options[0]['value'],
+            'hf_previous_page': 'SEARCH'
+            }
+            form.update(self.update_form(soup, update=False, additional_keys=['hf_course_offer_nbr', 'hf_course_id']))
+            
+            with closing(self.sess.post(self.course_url, headers=self.headers, data=form)) as res:
+                soup = BeautifulSoup(res.text, 'html.parser')
+                course_detail.update({
+                    'outcome': soup.select_one('#uc_course_outcome_lbl_learning_outcome').text,
+                    'syllabus': soup.select_one('#uc_course_outcome_lbl_course_syllabus').text,
+                    'required_readings': soup.select_one('#uc_course_outcome_lbl_req_reading').text,
+                    'recommended_readings': soup.select_one('#uc_course_outcome_lbl_rec_reading').text,
+                })
+                assessment_nodes = soup.select_one('#uc_course_outcome_gv_ast').findChildren('tr', recursive=False) # if no node, will raise error and return
+                assessment_nodes.pop(0)
+                assessments = {}
+                for tr in assessment_nodes:
+                    td_nodes = tr.select('td')
+                    assessments[td_nodes[1].text] = td_nodes[2].text
+                course_detail['assessments'] = assessments
         except AttributeError:
-            print('No section for this course')
+            print('No section / outcome for this course')
             pass
         return course_detail
+
+    def parse_sections(self, soup: BeautifulSoup) -> dict:
+        course_sections = {}
+        course_sections_table = soup.select_one('#uc_course_gv_sched').findChildren('tr', recursive=False)
+        course_sections_table.pop(0) # remove the header node
+        for schedule in course_sections_table:
+            # schedule is a <tr> tag with 3 children, first is the section code, second is the reg status, last is course detail table
+            children = schedule.findChildren('td', recursive=False)
+            section = children[0].text.strip('\n')
+            start_times, end_times, days, locations, instructors, meeting_dates = [], [], [], [], [], ''
+            timeslots = children[2].findChildren('tr') # each tr is a teaching timeslot, e.g. Wed and Thu Lectures are two teaching timelot
+            for node in timeslots:
+                details = list(filter(lambda x: x!='\n', node.get_text(';').split(';'))) # 0: days & time 1: Room 2: Instructor 3: part of teaching date
+                days_and_times = self.parse_days_and_times(details[0])
+                if days_and_times[0] in days and days_and_times[1] in start_times: # i.e. duplicated
+                    continue
+                days.append(days_and_times[0])
+                start_times.append(days_and_times[1])
+                end_times.append(days_and_times[2])
+                locations.append(details[1])
+                instructors.append(details[2])
+                meeting_dates += f', {details[3]}'
+            course_sections[section] = {
+                'startTimes': start_times,
+                'endTimes': end_times,
+                'days': days,
+                'locations': locations,
+                'instructors': instructors
+            }
+        return course_sections
+
 
     @staticmethod
     def parse_days_and_times(s: str) -> tuple:
@@ -175,11 +227,23 @@ class Course:
             'Sa': 6,
             'Su': 0,
         }
-        raw_list = list(filter(lambda x: x!='-', s.split())) # first is 2-letter weekday abbr, second is start time, last is end time
-        return (days_dict[raw_list[0]], to_24_hours(raw_list[1]), to_24_hours(raw_list[2]))
+        raw = list(filter(lambda x: x!='-', s.split())) # first is 2-letter weekday abbr, second is start time, last is end time
+        return (days_dict[raw[0]], to_24_hours(raw[1]), to_24_hours(raw[2]))
 
 cusis = Course()
-cusis.get_code_list()
-print(cusis.code_list)
+# cusis.get_code_list()
+# print(len(cusis.code_list))
+
+""" To get all courses for all subjects
+for code in cusis.code_list:
+    cusis.search_subject(code.split()[0], save=True)
+"""
 cusis.search_subject('ACCT', save=True)
 # print(cusis.courses)
+
+"""
+TODO
+1. Push to DB
+2. replace special char in outcome and syllabus with sth normal
+3. Auto captcha
+"""
