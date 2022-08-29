efbbfbebeb0e6f4899ea19a2707e79d22a4e0a8f
mikezzb <mikezhoudev@gmail.com>
2022-08-29 18:39:59 +0800

    Fix current term

--- __init__.py
+++ __init__.py
@@ -23,13 +23,11 @@ MAX_AUTO_CAPTCHA_ATTEMPTS = 16
 # General
 FLUSH = '\x1b[1K\r'
 
-# Manual Update Here
-CURRENT_TERM = "2022-23 Term 1"
-
 class CourseScraper:
-    def __init__(self, dirname='data', course_dirname='courses', derived_dirname='derived', statics_dirname='statics', resources_dirname='resources', save_captchas=True, timestamp: Union[str, bool]=False):
+    def __init__(self, current_term="2022-23 Term 1", dirname='data', course_dirname='courses', derived_dirname='derived', statics_dirname='statics', resources_dirname='resources', save_captchas=True, timestamp: Union[str, bool]=False):
         now = str(int(time.time())) if type(timestamp) is bool else timestamp
         self.dir_prefix = os.path.join(dirname, now)
+        self.current_term = current_term
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
@@ -133,7 +131,7 @@ class CourseScraper:
                     avaliable = False
                     if 'terms' in course:
                         for term in course['terms'].keys():
-                            if term == CURRENT_TERM:
+                            if term == self.current_term:
                                 avaliable = True
                                 break
                     if avaliable:
